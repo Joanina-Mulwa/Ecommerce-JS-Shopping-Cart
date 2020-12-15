d885548566c15b46ae7283ed0f3283ef4ede8cd7
Mulwa Joanina <joaninahmulwa@gmail.com>
2020-12-15 07:08:12 +0300

    Added to Cart

--- GriffinCodeChallenge.js
+++ GriffinCodeChallenge.js
@@ -10,7 +10,11 @@ const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 
 
+
+//cart
 let cart = [];
+//buttons
+let buttonsDOM = [];
 
 //getting Products
 class Products{
@@ -59,16 +63,95 @@ displayProducts(products){
             <h3>${product.title}</h3>
             <h4>$${product.price}</h4>
         </article>
-        `
+        `;
 
-    })
+    });
     productsDOM.innerHTML = result;
-}  
+} 
+
+getBagButtons(){
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+        let id = button.dataset.id;
+        let inCart = cart.find(item => item.id == id);
+        if(inCart){
+            button.innerText = "In Cart";
+            button.disabled = true;
+        }
+        
+            button.addEventListener("click" ,(event)=>{
+                event.target.innerText = "In Cart";
+                event.target.disabled = true;
+                //get product from products
+                let cartItem = {...Storage.getProducts(id),
+                amount: 1 };
+                //add product to cart 
+                cart = [...cart,cartItem];
+                //save cart in local storage
+                Storage.saveCart(cart);
+                //set cart values
+                this.setCartValues(cart);
+                //display card items
+                this.addCartItem(cartItem);
+                //show the cart
+                this.showCart();
+
+            });
+        
+    });
+}
+setCartValues(cart){
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+        tempTotal += item.price * item.amount;
+        itemsTotal += item.amount;
+
+    });
+
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+
+}
+addCartItem(item){
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = ` <img src=${item.image} alt="no image" />
+    <div>
+        <h4>${item.title}</h4>
+        <h5>$${item.price}</h5>
+        <span class="remove-item" data-id=${item.id}>remove</span>
+    </div>
+    <div>
+        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+        <p class="item-amount">${item.amount}</p>
+        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+    </div>`;
+    cartContent.appendChild(div);
+
+}
+showCart(){
+    cartOverLay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+
+}
 
 }
 
 //local storage
-class storage{
+class Storage{
+    static saveProducts(products){
+        localStorage.setItem("products",JSON.stringify(products));
+    }
+    static getProducts(id){
+        let products = JSON.parse(localStorage.getItem("products"));
+        return products.find(product => product.id == id)
+    }
+    static saveCart(cart){
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+
 
 }
 
@@ -79,7 +162,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const products = new Products();
 
     //get all products
-    products.getProducts().then(products => ui.displayProducts(products));
+    products.getProducts().then(products => {
+    ui.displayProducts(products)
+    Storage.saveProducts(products);
+  }).then(() => {
+      ui.getBagButtons();
 
-
+  });
 });
